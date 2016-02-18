bcdd70dd2be6322611c852c5528c1b3d1f3c28e2
Chris Foster <cdbfoster@gmail.com>
2016-02-18 14:18:23 -0500

    Added the first interactivity to the main menu.

--- src/view/main_menu.rs
+++ src/view/main_menu.rs
@@ -22,7 +22,8 @@ use sdl2::rect::Rect as SdlRectangle;
 
 use context::Context;
 use graphics::{GraphicObject, Sprite};
-use input::Input;
+use input;
+use input::{Button, Input};
 use math::{Vector2, Rectangle};
 use view::{View, ViewAction, ViewData};
 
@@ -38,6 +39,9 @@ pub struct MainMenuView {
 
     version: Sprite,
     author: Sprite,
+
+    highlighted: Option<usize>,
+    in_child_view: bool,
 }
 
 impl MainMenuView {
@@ -108,6 +112,9 @@ impl MainMenuView {
                 },
                 None => panic!("Could not render author text!"),
             },
+
+            highlighted: None,
+            in_child_view: false,
         }
     }
 }
@@ -132,7 +139,55 @@ impl View for MainMenuView {
     }
 
     fn update(&mut self, context: &mut Context, input: Vec<Input>, elapsed_ns: i64) -> (Option<ViewAction>, Vec<Input>) {
-        (None, Vec::new())
+        let mut menu_buttons: Vec<&mut MenuButton> = vec![
+            &mut self.play_button,
+            &mut self.options_button,
+            &mut self.scores_button,
+            &mut self.quit_button,
+        ];
+
+        let (highlighted, selected) = input::menu(
+            &menu_buttons.iter().map(|x| *x as &Button).collect(), // Get a list of immutable buttons
+            self.highlighted,
+            &input
+        );
+
+        self.highlighted = highlighted;
+        for (menu_index, menu_button) in menu_buttons.iter_mut().enumerate() {
+            match self.highlighted {
+                Some(ref index) => {
+                    if *index == menu_index {
+                        menu_button.hover = true;
+                    } else {
+                        menu_button.hover = false;
+                    }
+                },
+                None => { menu_button.hover = false; },
+            }
+        }
+
+        let view_action = match selected {
+            Some(ref index) => {
+                if *index == 0 {
+                    //Some(ViewAction::SetView(GameView::new(context))
+                    None
+                } else if *index == 1 {
+                    self.in_child_view = true;
+                    //Some(ViewAction::AddView(OptionsView::new(context))
+                    None
+                } else if *index == 2 {
+                    self.in_child_view = true;
+                    //Some(ViewAction::AddView(LeaderboardsView::new(context))
+                    None
+                } else {
+                    Some(ViewAction::ExitGame)
+                }
+            },
+            None => None,
+        };
+
+
+        (view_action, Vec::new())
     }
 }
 
@@ -226,6 +281,12 @@ impl MenuButton {
 
             hover: false,
         }
+    }
+}
+
+impl Button for MenuButton {
+    fn get_region(&self) -> Rectangle {
+        self.region
     }
 }
 
